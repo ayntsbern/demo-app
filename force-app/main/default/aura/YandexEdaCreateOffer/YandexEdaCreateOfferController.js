6582f0162508f81dcf4002697c2c5f091b11d91b
Yakushenkova <y.yakushenkova@ya.ru>
2019-09-20 16:14:59 +0300

    Done "save" for YandexEdaOffer page

--- force-app/main/default/aura/YandexEdaCreateOffer/YandexEdaCreateOfferController.js
+++ force-app/main/default/aura/YandexEdaCreateOffer/YandexEdaCreateOfferController.js
@@ -3,30 +3,28 @@
  */
 
 ({
-  // doInit: function(component, event, helper) {
-  //   let action = component.get("c.getInitData");
+  // init: function(component, event, helper) {
+  //   let action = component.get("c.hasOffer");
   //   action.setParams({
   //     "ID": component.get("v.recordId")
   //   });
-  //   //console.log(component.get("v.recordId"));
   //
   //   action.setCallback(this, function(response) {
-  //     //console.log(response.getReturnValue);
-  //     if (response.getState() === "SUCCESS") {
-  //       let initData = response.getReturnValue();
-  //       //console.log(initData.)
-  //       component.set("v.tableRows", initData);
-  //     }
+  //     if (response.getState() === "SUCCESS")
+  //       if (response.getState().getReturnValue() === true) {
+  //       component.set("v.hasErrors", true);
+  //     // } else {
+  //     //   var a = component.get('c.loadData');
+  //       }
   //   });
-  //   // Queue this action to send to the server
   //   $A.enqueueAction(action);
   // },
-  init: function (component, event, helper) {
+
+  init: function(component, event, helper) {
     let action = component.get("c.getInitData");
     action.setParams({
       "ID": component.get("v.recordId")
     });
-
 
 
     action.setCallback(this, function(response) {
@@ -38,20 +36,20 @@
           var row = rows[i];
           if (row.Account) row.AccountName = row.Account.Name;
         }
-        component.set('v.data', rows);
+        component.set("v.data", rows);
       }
     });
-    component.set('v.columns', [
-      {label: 'Контакт',  fieldName: 'Name',  type: 'text'},
-      {label: 'Ресторан', fieldName: 'AccountName',  type: 'text'},
-      {label: 'Email',    fieldName: 'Email', type: 'email'},
+    component.set("v.columns", [
+      { label: "Контакт", fieldName: "Name", type: "text" },
+      { label: "Ресторан", fieldName: "AccountName", type: "text" },
+      { label: "Email", fieldName: "Email", type: "email" }
     ]);
     $A.enqueueAction(action);
   },
 
-  updateSelectedText: function (component, event) {
-    var selectedRow = event.getParam('selectedRows');
-    component.set("v.currIdContact", selectedRow[0].Id);
+  updateSelectedText: function(component, event) {
+    var selectedRow = event.getParam("selectedRows");
+    component.set("v.currIDContact", selectedRow[0].Email);
   },
 
   createNewOffer: function(component, event, helper) {
@@ -60,23 +58,29 @@
       "accID": component.get("v.recordId")
     });
     action.setCallback(this, function(response) {
-      if (response.getReturnValue()){
+      if (response.getReturnValue()) {
         component.set("v.hasConfirm", true);
         component.set("v.hasErrors", false);
+
+        // let email = component.get("v.sendEmail");
+        // email.setParams({
+        //   "contactId": component.get("v.currIDContact"),
+        //   "accID" : component.get("v.recordId")
+        // });
+        // email.setCallback(this, function(emailResponse) {
+        //   component.set("v.offerLink", emailResponse.getReturnValue());
+        // });
+        event.getSource().set("v.disabled", true);
       } else {
         component.set("v.hasErrors", true);
         component.set("v.hasConfirm", false);
       }
     });
 
-    let email = component.get("v.sendEmail");
-    email.setParams({
-      "contactId": 'MLLLLLya'
-    });
     $A.enqueueAction(action);
   },
 
-  cancel : function(component, event, helper) {
+  cancel: function(component, event, helper) {
     $A.get("e.force:closeQuickAction").fire();
   }
 });
